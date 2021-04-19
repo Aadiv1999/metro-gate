f9982195e205a6005069406dcf74c77d8eac7313
Aadiv Shah <60790497+Aadiv1999@users.noreply.github.com>
2021-04-19 15:01:49 +0530

    Add files via upload

--- Access_Control.ino
+++ Access_Control.ino
@@ -1,10 +1,17 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Servo.h>
+
 Servo servo1;
-int servoPin = 6;
+Servo servo2;
+int servoPin1 = 6;
+int servoPin2 = 7;
+
+int ledpin = 13;
+
 #define SS_PIN 10
 #define RST_PIN 9
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
 void setup()
@@ -14,8 +21,13 @@ void setup()
   mfrc522.PCD_Init();   // Initiate MFRC522
   Serial.println("Approximate your card to the reader...");
   Serial.println();
-  servo1.attach(servoPin);
+
+  pinMode(ledpin,OUTPUT);
+
+  servo1.attach(servoPin1);
+  servo2.attach(servoPin2);
   servo1.writeMicroseconds(1500);
+  servo2.writeMicroseconds(1500);
 
 }
 void loop()
@@ -48,13 +60,16 @@ void loop()
   if (content.substring(1) == "47 6E D7 64") //enter the correct token here
   {
     Serial.println("Authorized access");
+    digitalWrite(ledpin,HIGH);
     servo1.writeMicroseconds(2500);
+    servo2.writeMicroseconds(2500);
     Serial.println();
     delay(2000);
   }
 
   else   {
     servo1.writeMicroseconds(500);
+    servo2.writeMicroseconds(500);
     Serial.println(" Access denied");
     delay(2000);
   }
